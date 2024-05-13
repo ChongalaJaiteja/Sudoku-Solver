2e66653e22e7a18d11660e0921f1950d16388201
ch.jai teja <chongalateja1234@gmail.com>
2024-05-13 18:56:07 +0530

    changed data store logic for sudoku cell data

--- src/components/sudokuSolver.jsx
+++ src/components/sudokuSolver.jsx
@@ -8,15 +8,14 @@ const SudokuSolver = () => {
     const [boardSize, setBoardSize] = useState(9);
     const boardSizes = [4, 6, 8, 9, 10, 12, 14, 15, 16];
     const initialBoard = Array.from({ length: boardSize }, () =>
-        Array.from({ length: boardSize }, () => ""),
+        Array.from({ length: boardSize }, () => ({
+            value: "",
+            isValid: true,
+            isEmpty: true,
+        })),
     );
-    const [solvedCells, setSolvedCells] = useState([]);
     const [isBoardSolved, setIsBoardSolved] = useState(false);
     const [board, setBoard] = useState(initialBoard);
-
-    const notify = () => {
-        toast.error("🦄invalid board !");
-    };
 
     // Function to calculate the submatrix dimensions
     const calculateSubmatrixDimensions = (n) => {
@@ -35,15 +34,50 @@ const SudokuSolver = () => {
     // Get the submatrix dimensions
     const { n, m } = calculateSubmatrixDimensions(boardSize);
 
-    // console.log("n", n, "m", m);
+    const handleCellChange = (event, i, j) => {
+        console.log("change event");
+        event.preventDefault();
+        const newValue = parseInt(event.target.value, 10);
+        if (event.target.value === "") {
+            setBoard((prevBoard) => {
+                const newBoard = [...prevBoard];
+                newBoard[i][j].value = "";
+                newBoard[i][j].isEmpty = true;
+                newBoard[i][j].isValid = true;
+                return newBoard;
+            });
+        } else if (newValue >= 1 && newValue <= boardSize) {
+            setBoard((prevBoard) => {
+                const newBoard = [...prevBoard];
+                newBoard[i][j].value = newValue.toString();
+                newBoard[i][j].isEmpty = false;
+                newBoard[i][j].isValid = true;
+                return newBoard;
+            });
+        }
+    };
+
+    const getCellClassNames = (i, j) => {
+        const classNames = [
+            "xs:w-12 h-10 min-w-9 border-[1px] border-gray-200 text-center sm:h-12 sm:w-14 md:h-14 md:w-16 text-black",
+            `${(i + 1) % n === 0 && i !== boardSize - 1 ? "border-b-2 border-b-gray-300" : ""}`,
+            `${(j + 1) % m === 0 && j !== boardSize - 1 ? "border-r-2 border-r-gray-300" : ""}`,
+            `${!board[i][j].isValid ? "bg-red-500 text-white" : ""}`,
+            `${isBoardSolved && board[i][j].isEmpty ? "bg-red-300" : ""}`,
+            // ` ${isEmptyCell ? (isEmptyCell.isValid ? (isBoardSolved ? "bg-red-300" : "bg-white") : "bg-red-500 text-white") : ""}`,
+        ];
+
+        return twMerge(classNames.filter((className) => className).join(" "));
+    };
+
     // Function to check if the number is valid
     const isValid = (board, row, col, c) => {
         for (let i = 0; i < boardSize; i++) {
             let rowValue = n * Math.floor(row / n) + Math.floor(i / m);
             let colValue = m * Math.floor(col / m) + (i % m);
-            if (board[i][col] == c) return false;
-            if (board[row][i] == c) return false;
-            if (board[rowValue][colValue] == c) return false;
+            if (board[i][col].value == c) return false;
+            if (board[row][i].value == c) return false;
+            if (board[rowValue][colValue].value == c) return false;
         }
         return true;
     };
@@ -52,16 +86,16 @@ const SudokuSolver = () => {
     const solve = (board, skips = false) => {
         for (let i = 0; i < board.length; i++) {
             for (let j = 0; j < board[0].length; j++) {
-                if (board[i][j] == "") {
+                if (board[i][j].value == "") {
                     const maxRange = boardSize.toString();
                     for (let c = "1"; c <= maxRange; c++) {
                         if (isValid(board, i, j, c)) {
-                            board[i][j] = c.toString();
+                            board[i][j].value = c.toString();
                             if (solve(board)) {
                                 if (skips) continue;
                                 return true;
                             } else {
-                                board[i][j] = "";
+                                board[i][j].value = "";
                             }
                         }
                     }
@@ -72,128 +106,81 @@ const SudokuSolver = () => {
         return true;
     };
 
-    // Function to verify the input board
-    const verifyInputBoard = (board, row, col, c) => {
-        for (let i = 0; i < board.length; i++) {
-            let rowValue = n * Math.floor(row / n) + Math.floor(i / m);
-            let colValue = m * Math.floor(col / m) + (i % m);
-            if (i != row && board[i][col] == c) return false;
-            if (i != col && board[row][i] == c) return false;
-            if (
-                rowValue != row &&
-                colValue != col &&
-                board[rowValue][colValue] == c
-            )
-                return false;
-        }
-        return true;
-    };
-
-    // Function to solve the sudoku
-    const solveSudoku = () => {
-        const newBoard = [...board];
-        const invalidCells = [];
-        // let validBoard = true;
-        setIsBoardSolved(false);
-        for (let i = 0; i < newBoard.length; i++) {
-            for (let j = 0; j < newBoard[0].length; j++) {
-                if (newBoard[i][j] == "") continue;
-                if (!verifyInputBoard(newBoard, i, j, newBoard[i][j])) {
-                    invalidCells.push(`${i}${j}`);
-                }
-            }
-        }
-        // FIXME: Fix the issue with the invalid cells include is not working
-        if (invalidCells.length > 0) {
-            setSolvedCells((prev) =>
-                prev.map((cell) =>
-                    invalidCells.includes(cell.position)
-                        ? { ...cell, isValid: false }
-                        : cell,
-                ),
-            );
-            notify();
-            return;
-        }
-        solve(newBoard);
-        setBoard(newBoard);
-        setIsBoardSolved(true);
-    };
-
     // Function to generate a random number
     //TODO:change min default value
     const randomGenerator = (min = 40, max = boardSize * boardSize) => {
         return Math.floor(Math.random() * max + min);
     };
 
-    const getEmptyCells = (inputBoard) => {
-        const emptyCells = [];
-        inputBoard.forEach((row, i) => {
-            row.forEach((col, j) => {
-                if (col == "") {
-                    emptyCells.push({ position: `${i}${j}`, isValid: true });
-                }
-            });
-        });
-
-        return emptyCells;
-    };
     // Function to regenerate the board
     const handleRegenerate = () => {
-        // setSolvedCells([]);
         setIsBoardSolved(false);
         let eraseNumbers = randomGenerator();
-        const newBoard = initialBoard;
+        const newBoard = [...initialBoard];
         solve(newBoard, true);
         for (let i = 0; i < newBoard.length; i++) {
             for (let j = 0; j < newBoard[0].length; j++) {
                 if (randomGenerator(0, n) == 0 && eraseNumbers > 0) {
-                    newBoard[i][j] = "";
+                    newBoard[i][j].value = "";
+                    newBoard[i][j].isEmpty = true;
                     eraseNumbers--;
+                }
+                if (newBoard[i][j].value !== "") {
+                    newBoard[i][j].isEmpty = false;
                 }
             }
         }
         setBoard(newBoard);
     };
 
-    // Function to handle cell change
-    const handleCellChange = (event, i, j) => {
-        event.preventDefault();
-        const newValue = parseInt(event.target.value, 10);
-        if (event.target.value === "") {
-            setBoard((prevBoard) => {
-                const newBoard = [...prevBoard];
-                newBoard[i][j] = "";
-                return newBoard;
-            });
-        } else if (newValue >= 1 && newValue <= boardSize) {
-            setBoard((prevBoard) => {
-                const newBoard = [...prevBoard];
-                newBoard[i][j] = newValue.toString(); // Convert back to string for consistency
-                return newBoard;
-            });
+    // Function to verify the input board
+    const verifyInputBoard = (board, row, col, c) => {
+        for (let i = 0; i < board.length; i++) {
+            let rowValue = n * Math.floor(row / n) + Math.floor(i / m);
+            let colValue = m * Math.floor(col / m) + (i % m);
+            if (i != row && board[i][col].value == c) return false;
+            if (i != col && board[row][i].value == c) return false;
+            if (
+                rowValue != row &&
+                colValue != col &&
+                board[rowValue][colValue].value == c
+            )
+                return false;
         }
+        return true;
     };
 
-    // Function to handle board reset
+    const notify = () => {
+        toast.error("🦄invalid board !");
+    };
+
+    // Function to solve the sudoku
+    const solveSudoku = () => {
+        const newBoard = [...board];
+        let isValidSudoku = true;
+        setIsBoardSolved(false);
+        for (let i = 0; i < newBoard.length; i++) {
+            for (let j = 0; j < newBoard[0].length; j++) {
+                newBoard[i][j].isValid = true;
+                if (newBoard[i][j].value == "") continue;
+                if (!verifyInputBoard(newBoard, i, j, newBoard[i][j].value)) {
+                    newBoard[i][j].isValid = false;
+                    isValidSudoku = false;
+                }
+            }
+        }
+        setBoard(newBoard);
+        if (!isValidSudoku) {
+            notify();
+            return;
+        }
+        solve(newBoard);
+        setIsBoardSolved(true);
+    };
+
     const handleReset = () => {
         setBoard(initialBoard);
         setIsBoardSolved(false);
-        // setSolvedCells([]);
-    };
-
-    const getCellClassNames = (i, j) => {
-        const classNames = [
-            "xs:w-12 h-10 min-w-9 border-[1px] border-gray-200 text-center sm:h-12 sm:w-14 md:h-14 md:w-16",
-            `${(i + 1) % n === 0 && i !== boardSize - 1 ? "border-b-2 border-b-gray-300" : ""}`,
-            `${(j + 1) % m === 0 && j !== boardSize - 1 ? "border-r-2 border-r-gray-300" : ""}`,
-            // `${solvedCells.find(cell => cell.position === `${i}${j}))?.isValid ? "" : "bg-red-200"}`,
-        ];
-        // console.log("solvedCells", solvedCells);
-        // console.log(
-        //     solvedCells.find((cell) => cell.position == `${i}${j}`)?.isValid,
-        // );
-        return twMerge(classNames.filter((className) => className).join(" "));
     };
 
     useEffect(() => {
@@ -201,13 +188,9 @@ const SudokuSolver = () => {
         handleRegenerate();
     }, [boardSize]);
 
-    useEffect(() => {
-        setSolvedCells(getEmptyCells(board));
-    }, [board]);
-
     return (
         <>
-            <nav className="fixed inset-0 bottom-auto  flex items-center bg-red-300 px-2 py-4 shadow sm:px-5 md:px-8 md:shadow-lg xl:px-10 ">
+            <nav className="fixed inset-0 bottom-auto  flex items-center bg-red-300 px-2 py-4 shadow sm:px-5 md:px-8 md:shadow-lg xl:px-10">
                 <SideBar
                     sideBarItems={boardSizes}
                     onChangeBoardSize={(size) => setBoardSize(size)}
@@ -218,7 +201,7 @@ const SudokuSolver = () => {
             </nav>
 
             <div className="container">
-                <div className="my-24 flex w-full flex-col justify-center gap-y-8 overflow-x-auto outline">
+                <div className="my-24 flex w-full flex-col justify-center gap-y-8 overflow-x-auto outline ">
                     <ul className="flex flex-col self-center">
                         {board.map((row, i) => (
                             <li key={i} className="flex" id={i}>
@@ -227,7 +210,7 @@ const SudokuSolver = () => {
                                         key={`${i}${j}`}
                                         type="number"
                                         min="1"
-                                        value={row[j]}
+                                        value={col.value}
                                         max={boardSize}
                                         disabled={isBoardSolved}
                                         onChange={(event) =>
